8293e0fa60bc7b0902f4207abc5492acc02a1b08
Arshad Khan <arshad910khan@gmail.com>
2026-09-14 21:39:22 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.29%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        while (n>0){
+            int c=a+b;
+            a=b;
+            b=c;
+            n--;
+        }
+        return a;
+    }
+}
